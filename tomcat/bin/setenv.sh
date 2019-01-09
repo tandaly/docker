01dd4cdf6153968ddec4158d96ea88c8858830c4
tanf <tandaly@qq.com>
2019-01-09 13:50:49 +0800

    update

--- tomcat/bin/setenv.sh
+++ tomcat/bin/setenv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # JAVA-OPTIONS
-JAVA_OPTS="$JAVA_OPTS -Xms128m -Xmx${heap_size}m -XX:MaxPermSize=256m -server"
+JAVA_OPTS="$JAVA_OPTS -Xms128m -XX:MaxPermSize=256m -server"
 # CATALINA-OPTIONS
 CATALINA_OPTS="$CATALINA_OPTS -DserverType=rdtest -DserverName=first"
 
