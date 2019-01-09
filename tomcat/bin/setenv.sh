b28dcdb5e4e153af3b4f695bedb6527c314bda4d
tanf <tandaly@qq.com>
2019-01-09 14:38:21 +0800

    update

--- tomcat/bin/setenv.sh
+++ tomcat/bin/setenv.sh
@@ -8,4 +8,5 @@
 JAVA_OPTS="$JAVA_OPTS -Duser.timezone=Asia/Shanghai"
 #probe 
 JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote"
-JAVA_OPTS="$JAVA_OPTS -Xms1024m -Xmx1024m -XX:PermSize=128M -XX:MaxPermSize=256m -Dfile.encoding=UTF8 -Dsun.jnu.encoding=UTF8 -Djava.security.egd=file:/dev/./urandom"
+#JAVA_OPTS="$JAVA_OPTS -Xms1024m -Xmx1024m -XX:PermSize=128M -XX:MaxPermSize=256m -Dfile.encoding=UTF8 -Dsun.jnu.encoding=UTF8 -Djava.security.egd=file:/dev/./urandom"
+JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8 -Dsun.jnu.encoding=UTF8 -Djava.security.egd=file:/dev/./urandom"
