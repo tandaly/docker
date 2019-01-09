ca51dd266fb049a15a482b5b78756b8e1a52d472
tanf <tandaly@qq.com>
2019-01-09 14:40:53 +0800

    update

--- tomcat/entrypoint.sh
+++ tomcat/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#chmod +x entrypoint.sh
 limit_in_bytes=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
 
 # If not default limit_in_bytes in cgroup
@@ -6,7 +7,7 @@ if [ "$limit_in_bytes" -ne "9223372036854771712" ]
 then
     limit_in_megabytes=$(expr $limit_in_bytes \/ 1048576)
     heap_size=$(expr $limit_in_megabytes - $RESERVED_MEGABYTES)
-    export JAVA_OPTS="-Xmx${heap_size}m $JAVA_OPTS"
+    export JAVA_OPTS="-Xms${heap_size}m -Xmx${heap_size}m $JAVA_OPTS"
     echo JAVA_OPTS=$JAVA_OPTS
 fi
 
